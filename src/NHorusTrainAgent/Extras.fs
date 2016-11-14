d0fb92c8beb7fd457c6e1719cbb7aa0b565364e4
Artemy <artemy.bordushko@hotmail.com>
2016-11-14 09:11:33 +0300

    Route checkpoint parsing implemented (not tested yet). Some code restructuring.

--- src/NHorusTrainAgent/Extras.fs
+++ src/NHorusTrainAgent/Extras.fs
@@ -10,13 +10,13 @@ type Relation =
     | Rrel of ScElement
     | Nrel of ScElement
 
-
 type El3<'a, 'b> = 'a * ScTypes * 'b
 type El5<'a, 'b, 'c> = 'a * ScTypes * 'b * ScTypes * 'c
 
 type ScConstruction<'a, 'b, 'c> =
     | El3 of El3<'a, 'b>
     | El5 of El5<'a, 'b, 'c>
+
 
 let inline (==>) e1 e2 =
     (e1, ScTypes.ArcCommon, e2)
@@ -29,4 +29,29 @@ let inline (|<-) (c3 :El3<'a,'b>) (rel :Relation) =
     match rel with
     | Rrel addr -> (a,b,c, ScTypes.ArcCommon, addr)
     | Nrel addr -> (a,b,c, ScTypes.ArcAccessConstantPositivePermanent , addr)
- 
+
+
+
+open FSharpx
+open FSharpx.Option
+
+
+let tryGetElement i (constr :ScConstruction) =
+    constr
+    //|> Seq.head
+    |> fun x -> x.[i]
+    |> fun x -> if x.IsValid then Some x else None
+
+
+let tryGetNode i constr =
+    tryGetElement i constr
+    >>= function    | :? ScNode as n -> Some n
+                    | _ -> None
+    
+let tryFetchLinkContent (element :ScElement) =
+    maybe {
+        let! link = match element with
+                    | :? ScLink as l -> Some l
+                    | _ -> None
+        return link.LinkContent
+    }
--- src/NHorusTrainAgent/Main.fs
+++ src/NHorusTrainAgent/Main.fs
@@ -1,48 +1,109 @@
 module NHorusTrainAgent
 
+open System
 open ScEngineNet.Extras
 open ScEngineNet
 open ScEngineNet.ScElements
 open ScMemoryNet
+open ScEngineNet.LinkContent
+open FSharpx
+open FSharpx.Option
 
 
-type ConstructionTemplates(ctx :ScMemoryContext) =
+type RoutePoint = {
+    Id          : string
+    Departure   : TimeSpan
+    Arrival     : TimeSpan
+}
+
+    
+
+type KbTraverse(ctx :ScMemoryContext) =
+    
+    let stations = System.Collections.Generic.List<RoutePoint>()
 
     let node idStr =
-        ctx.FindNode(Identifier(idStr)) :> ScElement
-    
-    let hours (root :ScElement) =
-        (root --> ScTypes.Link) |<- (Rrel (node "rrel_hours"))
-        |> ctx.CreateIterator
+        ctx.FindNode(Identifier(idStr)) :> ScElement    
 
-    let minutes (root :ScElement) =
-        (root --> ScTypes.Link) |<- (Rrel (node "rrel_minutes"))
-        |> ctx.CreateIterator
 
-    let value (eIn :ScElement) (eOut :ScElement) =
-        (eIn ==> eOut) |<- (Nrel (node "nrel_value"))
-        |> ctx.CreateIterator
-
-    let time (timeType :string) (root :ScElement) =
-        (root ==> ScTypes.Node) |<- (Nrel (node timeType))
-        |> ctx.CreateIterator
-    let departureTime = time "nrel_departure_time"
-    let arrivalTime = time "nrel_arrival_time"
-
-    let rwStation stationId =
-        (ScTypes.NodeConstant ==> (node stationId)) |<- (Nrel (node "nrel_rw_station"))
+    let tryValue (root :ScElement) =
+        (ScTypes.NodeConstant ==> root) |<- (Nrel (node "nrel_value"))
         |> ctx.CreateIterator
         |> Seq.head
-        |> fun x -> x.[0]
-        |> fun x -> if x.IsValid then Some x else None
-    
-    let isRwRouteCheckPoint (root :ScElement) =
-        (node "concept_rw_route_check_point" --> root)
+        |> tryGetNode 0
+
+
+    let tryGetTime (time :ScNode) =
+        let tryGetTimeUnits rrelId (root :ScElement) =
+            (root --> ScTypes.Link) |<- (Rrel (node rrelId))
+            |> ctx.CreateIterator
+            |> fun itr -> maybe {
+                    let! lContent = Seq.head itr
+                                    |> tryGetElement 2
+                                    >>= tryFetchLinkContent
+                    let v = lContent :?> ScInt32
+                    return v.Value
+                }
+        let tryGetHours = tryGetTimeUnits "rrel_hours"
+        let tryGetMinutes = tryGetTimeUnits "rrel_minutes"
+        
+        maybe {
+            let! h_min = tryValue time
+            let! h = tryGetHours h_min
+            let! min = tryGetMinutes h_min
+            return TimeSpan(h, min, 0)
+        }
+
+
+    let tryTime (timeId :string) (checkPoint :ScNode) =
+        (checkPoint ==> ScTypes.NodeConstant) |<- (Nrel (node timeId))
         |> ctx.CreateIterator
+        |> Seq.head
+        |> tryGetNode 2
+        
+    let tryDepartureTime = tryTime "nrel_departure_time"
+    let tryArrivalTime = tryTime "nrel_arrival_time"
+
+
+    let getRwCheckPoints (station :ScNode) =
+        (ScTypes.NodeConstant ==> station) |<- (Nrel (node "nrel_rw_station"))
+        |> ctx.CreateIterator
+        |> Seq.map (tryGetNode 0)
+        |> Seq.filter Option.isSome
+        |> Seq.map Option.get
     
-    let nextPoint (root :ScElement) =
+    let isRwRouteCheckPoint (x :ScNode) =
+        (node "concept_rw_route_check_point" --> x)
+        |> ctx.CreateIterator
+        |> (Seq.isEmpty >> not)
+    
+    let tryGetNextPoint (root :ScElement) =
         (root ==> ScTypes.NodeConstant) |<- (Nrel (node "nrel_next_point"))
         |> ctx.CreateIterator
+        |> Seq.head
+        |> tryGetNode 2
+
+
+    let getRoutePoint (station :ScNode) (chPoint :ScNode) =
+        maybe {
+            let! departure = tryDepartureTime chPoint
+                                >>= tryGetTime
+            let! arrival = tryArrivalTime chPoint
+                                >>= tryGetTime
+            return { Id = station.SystemIdentifier.Value
+                     Departure = departure;
+                     Arrival = arrival }
+        }
+
+    let findStations (station :ScNode) (departure :TimeSpan) (hours :int) =
+        let maxTime = departure + TimeSpan(hours, 0, 0)
+
+        getRwCheckPoints station
+        |> Seq.map (getRoutePoint station)
+        |> Seq.filter Option.isSome
+        |> Seq.map Option.get
+        
+
         
 let initializeScMemory () =
     let parameters = ScMemoryParams(true,
@@ -52,6 +113,8 @@ let initializeScMemory () =
                         netExtensionsPath = Config.NetExtensionPath)
     if (not ScMemory.IsInitialized)
         then ScMemory.Initialize(parameters)
+
+
 
 [<EntryPoint>]
 let main argv =
